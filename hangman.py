dda01bd6f8b48346989634d91621e676237e20e2
Julian <julian.a.garza11@gmail.com>
2018-10-30 22:16:59 +0000

    Added while loop and counted errors.

--- hangman.py
+++ hangman.py
@@ -45,3 +45,23 @@ def draw_hangman(num_wrong_guesses):
         output = output + "\n |"
     output = output + "____\n\n"
     print(output)
+
+stored_name = input("What is your name? ")
+print("Hello {}! It's time to play hangman".format(stored_name))
+
+find_word = "test"
+
+errors = 0
+while errors < 6:
+    guess = input("Guess a letter : ")
+
+    if guess.lower() in find_word:
+        print("Good Job!")
+    else:
+        print("Try again. You failed at life.")
+        errors = errors + 1
+        print("errors = {}".format(errors))
+
+
+
+
